87527dec47555e8d525f2902655630a7b761cd6d
akila <akila.aki28@gmail.com>
2014-01-21 16:37:35 +0530

    customer id

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.14'
 gem 'bootstrap-sass', '2.1'
 gem 'jquery-rails', '2.0.2'
 gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.6'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -35,6 +36,7 @@ end
 
 group :test do 
   gem 'capybara', '1.1.2'
+  gem 'factory_girl_rails', '4.1.0'
 end
 
 group :production do
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -25,6 +25,11 @@ before_save :set_default_val
     self.status = 'Pending' unless self.status
  end
 
+before_create :increment_code
+def increment_code
+    self.code = (self.class.last.nil?) ? "0" : ((self.class.last.code.to_i) + 0000000001).to_s
+end
+
 validates :first_name, presence: true,
 		       format: { with: %r{^[a-zA-Z]*} }
 validates :last_name, presence: true,
@@ -40,9 +45,9 @@ validates_inclusion_of :date_of_birth,
                 :in => Date.civil(1900,1,1)..Date.today,
                  :message => "is invalid"
 
-validates_uniqueness_of :code, presence: true, :wrong_length => "should have 10digits"
-validates_length_of :code, :is =>10, :start_code => 0000000001
-validates :code, numericality: { greater_than_or_equal_to: 0000000001 }
+#validates_uniqueness_of :code, presence: true, :wrong_length => "should have 10digits"
+#validates_length_of :code, :is =>10, :start_code => 0000000001
+#validates :code, numericality: { greater_than_or_equal_to: 0000000001 }
 
 validates :address, presence: true
 validates :sex, presence: true
