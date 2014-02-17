51ba38621b5a82c2809c3118fdc259fa0cf746ea
akila <akila.aki28@gmail.com>
2014-02-17 12:32:35 +0530

    folio number

--- db/migrate/20140215060643_remove_folio_number_from_folios.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFolioNumberFromFolios < ActiveRecord::Migration
-  def up
-    remove_column :folios, :folio_number
-  end
-
-  def down
-    add_column :folios, :folio_number, :integer
-  end
-end
