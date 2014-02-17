52fb7a58ca22f53c5f54172e9eaa6a7bb289309a
akila <akila.aki28@gmail.com>
2014-02-17 12:02:44 +0530

    folios

--- db/migrate/20140214062321_create_folios.rb
+++ db/migrate/20140214062321_create_folios.rb
@@ -2,7 +2,6 @@ class CreateFolios < ActiveRecord::Migration
   def change
     create_table :folios do |t|
       t.datetime :folio_allocation_date
-      t.string :folio_number
       t.integer :no_of_shares
       t.timestamps
     end
